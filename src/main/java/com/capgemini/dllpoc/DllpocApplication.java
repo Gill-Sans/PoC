98f1e92d6b659744660294a40621bd190fb94b19
apowrozn-capgemini <153419375+apowrozn-capgemini@users.noreply.github.com>
2025-10-23 20:28:19 +0200

    first commit

---
+++ src/main/java/com/capgemini/dllpoc/DllpocApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.dllpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DllpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DllpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/dllpoc/DllpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.dllpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DllpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
